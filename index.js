9c16d50b4868feceb0e8ce7a25fc3abf32555031
Priyanshu-Tyagi <manutyagi9991@gmail.com>
2022-11-04 12:07:39 +0530

    inital commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/",(req,res)=>{
     res.send("Home Page")
 })
 
+app.get("/about",(req,res)=>{
+    res.send("About Page")
+})
+
 app.listen(process.env.PORT,()=>{
     console.log(`listening on PORT ${process.env.PORT}`)
 })
